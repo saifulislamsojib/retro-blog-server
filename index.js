9a35f2e22e2a527f9a860b44c9184cd04e2e6706
saifulislamsojib <saifulislam.sojib56@gmail.com>
2023-01-23 20:57:05 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ObjectId } = require("mongodb");
 
 const app = express();
 
@@ -11,91 +11,92 @@ app.use(cors());
 const port = process.env.PORT || 4000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ernz8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-client.connect(err => {
-  const blogsCollection = client.db(process.env.DB_NAME).collection("blogs");
-
-  const adminCollection = client.db(process.env.DB_NAME).collection("admins");
-
-  app.get('/admins', (req, res) => {
-    adminCollection.find({})
-    .toArray((err, admins) => {
-      res.send(admins);
-    })
-  })
-
-  app.delete('/deleteAdmin', (req, res) => {
-
-  })
-
-  app.get('/isAdmin', (req, res) => {
-    const { email } = req.query;
-    adminCollection.find({ email })
-    .toArray((err, admin)=> {
-      if (admin.length){
-        res.send(true);
-      }
-      else{
-        res.send(false);
-      }
-    })
-  })
-
-  app.post('/addAdmin', (req, res) => {
-    const adminData = req.body;
-
-    adminCollection.insertOne(adminData)
-    .then(result => res.send({
-      inserted: result.insertedCount > 0,
-      _id: result.insertedId
-    }))
-  })
-
-  app.post('/addBlog', (req, res) => {
-    const blog = req.body;
-
-    blogsCollection.insertOne(blog)
-    .then(result => res.send({
-      inserted: result.insertedCount > 0,
-      _id: result.insertedId
-    }))
-  })
-
-  app.get('/blogs', (req, res)=> {
-    blogsCollection.find({})
-    .toArray((err, blog) => {
-      res.send(blog);
-    })
-  })
-
-  app.get('/blog/:id', (req, res)=> {
-    const { id } = req.params;
-    blogsCollection.find({_id: ObjectId(id)})
-    .toArray((err, blog) => {
-      res.send(blog[0]);
-    })
-  })
-
-  app.delete('/deleteBlog/:id', (req, res) => {
-    const { id } = req.params;
-    blogsCollection.deleteOne({_id: ObjectId(id)})
-    .then(result => res.send(result.deletedCount > 0))
-  })
-
-  app.patch('/updateBlog/:id', (req, res) => {
-    const { id } = req.params;
-    blogsCollection.updateOne({_id: ObjectId(id)}, {
-      $set : req.body,
-    })
-    .then((result) => res.send({inserted: result.modifiedCount > 0}))
-  })
-  
-  //client.close();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello Retro Blog!')
+(async () => {
+  const db = client.db(process.env.DB_NAME);
+  const blogsCollection = db.collection("blogs");
+  const adminCollection = db.collection("admins");
+
+  app.get("/admins", (req, res) => {
+    adminCollection.find({}).toArray((err, admins) => {
+      res.send(admins);
+    });
+  });
+
+  app.get("/isAdmin", (req, res) => {
+    const { email } = req.query;
+    adminCollection.find({ email }).toArray((_err, admin) => {
+      if (admin.length) {
+        res.send(true);
+      } else {
+        res.send(false);
+      }
+    });
+  });
+
+  app.post("/addAdmin", (req, res) => {
+    const adminData = req.body;
+
+    adminCollection.insertOne(adminData).then((result) =>
+      res.send({
+        inserted: result.acknowledged,
+        _id: result.insertedId,
+      })
+    );
+  });
+
+  app.post("/addBlog", (req, res) => {
+    const blog = req.body;
+
+    blogsCollection.insertOne(blog).then((result) =>
+      res.send({
+        inserted: result.acknowledged,
+        _id: result.insertedId,
+      })
+    );
+  });
+
+  app.get("/blogs", (req, res) => {
+    blogsCollection.find({}).toArray((_err, blog) => {
+      res.send(blog);
+    });
+  });
+
+  app.get("/blog/:id", (req, res) => {
+    const { id } = req.params;
+    blogsCollection.find({ _id: ObjectId(id) }).toArray((_err, blog) => {
+      res.send(blog[0]);
+    });
+  });
+
+  app.delete("/deleteBlog/:id", (req, res) => {
+    const { id } = req.params;
+    blogsCollection
+      .deleteOne({ _id: ObjectId(id) })
+      .then((result) => res.send(result.deletedCount > 0));
+  });
+
+  app.patch("/updateBlog/:id", (req, res) => {
+    const { id } = req.params;
+    blogsCollection
+      .updateOne(
+        { _id: ObjectId(id) },
+        {
+          $set: req.body,
+        }
+      )
+      .then((result) => res.send({ inserted: result.modifiedCount > 0 }));
+  });
+
+  //client.close();
+})().catch((err) => console.log(err));
+
+app.get("/", (_req, res) => {
+  res.send("Hello Retro Blog!");
 });
 
 app.listen(port, () => {
